3cf2698dbb1def9d78b084a118a3b1c5c6c53c43
Stellar-F0X <jms030907@gmail.com>
2025-10-29 01:41:47 +0900

    fix: 레이어 비교 실수 수정

--- Scripts/InputReceiver.cs
+++ Scripts/InputReceiver.cs
@@ -8,9 +8,10 @@ namespace InputLayer.Runtime
     public class InputReceiver : MonoBehaviour
     {
         private readonly Dictionary<string, Action<InputAction.CallbackContext>> _cachedCallbacks = new(StringComparer.Ordinal);
-        
+
         [SerializeField]
         private InputLayerName _activeLayer;
+
         private InputLayer _focusedInputLayer;
 
 
@@ -31,25 +32,57 @@ namespace InputLayer.Runtime
         }
 
 
+
+        private void OnDestroy()
+        {
+            foreach (KeyValuePair<string, Action<InputAction.CallbackContext>> pair in _cachedCallbacks)
+            {
+                string[] paths = pair.Key.Split('.');
+
+                if (paths.Length < 2)
+                {
+                    continue;
+                }
+                
+                InputAction action = _focusedInputLayer.GetAction(paths[0]);
+
+                if (action is null)
+                {
+                    continue;
+                }
+                
+                action.started -= pair.Value;
+                action.canceled -= pair.Value;
+                action.performed -= pair.Value;
+            }
+
+            _cachedCallbacks.Clear();
+        }
+
+
+
         public virtual void RegisterInputAction(string actionName, InputCallback callbackType, Action<InputAction.CallbackContext> callback)
         {
             InputAction action = _focusedInputLayer.GetAction(actionName);
 
             if ((callbackType & InputCallback.Started) > 0)
             {
-                _cachedCallbacks[actionName + InputCallback.Started] = callback;
+                string key = $"{actionName}.{InputCallback.Started}";
+                _cachedCallbacks[key] = callback;
                 action.started += callback;
             }
 
             if ((callbackType & InputCallback.Canceled) > 0)
             {
-                _cachedCallbacks[actionName + InputCallback.Canceled] = callback;
+                string key = $"{actionName}.{InputCallback.Canceled}";
+                _cachedCallbacks[key] = callback;
                 action.canceled += callback;
             }
 
             if ((callbackType & InputCallback.Performed) > 0)
             {
-                _cachedCallbacks[actionName + InputCallback.Performed] = callback;
+                string key = $"{actionName}.{InputCallback.Performed}";
+                _cachedCallbacks[key] = callback;
                 action.performed += callback;
             }
         }
@@ -58,32 +91,32 @@ namespace InputLayer.Runtime
         public virtual void UnregisterInputAction(string actionName, InputCallback callbackType)
         {
             InputAction action = _focusedInputLayer.GetAction(actionName);
-            
+
             if ((callbackType & InputCallback.Started) > 0)
             {
-                string key1 = actionName + InputCallback.Started;
-                action.started -= _cachedCallbacks.GetValueOrDefault(key1);
+                string key = $"{actionName}.{InputCallback.Started}";
+                action.started -= _cachedCallbacks.GetValueOrDefault(key);
             }
 
             if ((callbackType & InputCallback.Canceled) > 0)
             {
-                string key2 = actionName + InputCallback.Canceled;
-                action.canceled -= _cachedCallbacks.GetValueOrDefault(key2);
+                string key = $"{actionName}.{InputCallback.Canceled}";
+                action.canceled -= _cachedCallbacks.GetValueOrDefault(key);
             }
 
             if ((callbackType & InputCallback.Performed) > 0)
             {
-                string key3 = actionName + InputCallback.Performed;
-                action.performed -= _cachedCallbacks.GetValueOrDefault(key3);
+                string key = $"{actionName}.{InputCallback.Performed}";
+                action.performed -= _cachedCallbacks.GetValueOrDefault(key);
             }
         }
-        
-        
+
+
         public bool ReadInput<T>(in string actionName, out T value) where T : struct
         {
             InputManager manager = Singleton<InputManager>.Instance;
-            
-            if (manager.inputBlock || manager.peekInputLayer == _focusedInputLayer)
+
+            if (manager.inputBlock || manager.peekInputLayer != _focusedInputLayer)
             {
                 value = default;
                 return false;
@@ -93,25 +126,25 @@ namespace InputLayer.Runtime
             return true;
         }
 
-        
+
         public bool ReadButton(in string actionName)
         {
             InputManager manager = Singleton<InputManager>.Instance;
-            
-            if (manager.inputBlock || manager.peekInputLayer == _focusedInputLayer)
+
+            if (manager.inputBlock || manager.peekInputLayer != _focusedInputLayer)
             {
                 return false;
             }
 
             return _focusedInputLayer.GetAction(actionName)?.IsInProgress() ?? false;
         }
-        
-        
+
+
         public bool ReadButtonUp(string actionName)
         {
             InputManager manager = Singleton<InputManager>.Instance;
-            
-            if (manager.inputBlock || manager.peekInputLayer == _focusedInputLayer)
+
+            if (manager.inputBlock || manager.peekInputLayer != _focusedInputLayer)
             {
                 return false;
             }
@@ -123,8 +156,8 @@ namespace InputLayer.Runtime
         public bool ReadButtonDown(string actionName)
         {
             InputManager manager = Singleton<InputManager>.Instance;
-            
-            if (manager.inputBlock || manager.peekInputLayer == _focusedInputLayer)
+
+            if (manager.inputBlock || manager.peekInputLayer != _focusedInputLayer)
             {
                 return false;
             }
